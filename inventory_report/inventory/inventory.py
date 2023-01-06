d48f459dee81ac135ad201e1f16e816577f63ab6
Thayssa Bernardo <thayssabernardo@gmail.com>
2023-01-06 15:55:39 -0300

    Diminuindo complexidade do metodo, e funcionalidade de gerar relatorio atraves de arquivo json

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,14 +1,30 @@
 import csv
+import json
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
 
 class Inventory:
-    @staticmethod
-    def import_data(path: str, type: str):
+    @classmethod
+    def import_data(cls, path: str, type: str):
         if "csv" in path:
-            with open(path, encoding="utf-8") as file:
-                reader = list(csv.DictReader(file))
+            return cls.read_csv(path, type)
+        elif "json" in path:
+            return cls.read_json(path, type)
+
+    @classmethod
+    def read_csv(cls, path, type):
+        with open(path, encoding="utf-8") as file:
+            reader = list(csv.DictReader(file))
+            if (type == "simples"):
+                return SimpleReport.generate(reader)
+            elif (type == "completo"):
+                return CompleteReport.generate(reader)
+
+    @classmethod
+    def read_json(cls, path, type):
+        with open(path, encoding='utf-8') as file:
+            reader = list(json.load(file))
             if (type == "simples"):
                 return SimpleReport.generate(reader)
             elif (type == "completo"):
