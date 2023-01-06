34374631af33c5fdda217a676f9d1964313470aa
Thayssa Bernardo <thayssabernardo@gmail.com>
2023-01-06 14:46:51 -0300

    Relatorio completo

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,23 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+
+    def generate(list: list):
+        simple_report = SimpleReport.generate(list)
+
+        quantidade_empresa = Counter(
+            [empresa["nome_da_empresa"] for empresa in list]
+        )
+
+        empresas = ""
+
+        for key, value in quantidade_empresa.items():
+            empresas += f"- {key}: {value}\n"
+
+        return (
+            f"{simple_report}\n"
+            f"Produtos estocados por empresa:\n"
+            f"{empresas}"
+        )
