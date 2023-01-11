81cb6e1cb497b8f10f30cf7df9da1d522e2cabf6
Thayssa Bernardo <thayssabernardo@gmail.com>
2023-01-11 15:57:18 -0300

    Criando teste de relatório individual do produto

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,31 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = [
+        2,
+        "farinha",
+        "Farinini",
+        "01-05-2021",
+        "02-06-2023",
+        "A6B7C8",
+        "ao abrigo de luz"
+    ]
+
+    instance_product = Product(
+        id=product[0],
+        nome_do_produto=product[1],
+        nome_da_empresa=product[2],
+        data_de_fabricacao=product[3],
+        data_de_validade=product[4],
+        numero_de_serie=product[5],
+        instrucoes_de_armazenamento=product[6]
+    )
+
+    assert instance_product.__repr__() == (
+        f"O produto {product[1]}"
+        f" fabricado em {product[3]}"
+        f" por {product[2]} com validade"
+        f" até {product[4]}"
+        f" precisa ser armazenado {product[6]}."
+    )
